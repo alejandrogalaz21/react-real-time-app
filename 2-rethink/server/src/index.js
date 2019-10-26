3e0549df5b7b09595717e32fa3ec22819e2bb266
Alejandro Galaz <alejandrogalaz21@gmail.com>
2019-10-26 00:22:28 -0500

    add data

--- 2-rethink/server/src/index.js
+++ 2-rethink/server/src/index.js
@@ -1,39 +1,33 @@
-import socketIo from "socket.io"
-import rethinkdb from "rethinkdb"
+import socketIo from 'socket.io'
+import r from 'rethinkdb'
 
 const io = socketIo()
-const port = 8000
+const port = 3001
 
-rethinkdb
-  .connect({
-    host: "localhost",
-    port: 28015,
-    db: "awesome_whiteboard"
-  })
+r.connect({
+  host: 'localhost',
+  port: 28015,
+  db: 'awesome_whiteboard'
+})
   .then(connection => {
-    io.on("connection", client => {
-      client.on("subscribeToTimer", interval => {
+    io.on('connection', client => {
+      client.on('subscribeToTimer', interval => {
         console.log(`Client is subscribing to timer with interval ${interval}`)
 
-        rethinkdb
-          .table("timers")
+        r.table('timers')
           .changes()
           .run(connection)
           .then(cursor => {
             cursor.each((error, timerRow) => {
-              if (error) throw new error(error)
-              client.emit("timer", timerRow.new_val.timestamp)
+              if (error) console.error(error)
+              client.emit('timer', timerRow.new_val.timestamp)
             })
           })
-          .catch(error => {
-            throw new error(error)
-          })
+          .catch(error => console.error(error))
       })
     })
   })
-  .catch(error => {
-    throw new error(error)
-  })
+  .catch(error => console.error(error))
 
 io.listen(port)
 console.log(`Server Listing on port ${port}`)
